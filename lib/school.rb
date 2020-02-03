7438b72de6059bafe43af047ab390659ef2d76bf
Tommy Connor <tconnor98@aol.com>
2020-02-03 22:19:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,11 +1,15 @@
 class School
   attr_accessor :name, :roster
+  
   def initialize(name)
     @name = name
     @roster = {}
   end
+  
   def add_student(student, level)
     @roster[level] ||= []
     @roster[level] << student
   end
+  
+  def grade 
 end
